b59617e8c006b26dc139ad32bb3570f939193b8e
jeremiah <jeremiah@t430>
2018-09-11 19:16:32 -0400

    Added @SafeVarargs

--- src/net/net16/jeremiahlowe/shared/ArrayUtility.java
+++ src/net/net16/jeremiahlowe/shared/ArrayUtility.java
@@ -1,6 +1,7 @@
 package net.net16.jeremiahlowe.shared;
 
 public class ArrayUtility {
+	@SafeVarargs
 	public static <T> boolean anyOf(T any, T... of){
 		for(T t : of){
 			if(any == t) return true;
