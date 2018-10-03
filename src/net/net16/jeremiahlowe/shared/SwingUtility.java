6d9bbb9a55ad21a65e4ea5626bb2d009282a7ccd
jeremiah <jeremiah@linux-pc>
2018-10-03 01:50:06 -0400

    Added a SwingUtility class

---
+++ src/net/net16/jeremiahlowe/shared/SwingUtility.java
@@ -0,0 +1,16 @@
+package net.net16.jeremiahlowe.shared;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class SwingUtility {
+	public static final JFrame centerJFrame(JFrame f) {
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = ge.getDefaultScreenDevice();
+		Rectangle screen = gd.getDefaultConfiguration().getBounds();
+		int x = screen.x + screen.width / 2 - f.getWidth() / 2;
+		int y = screen.y + screen.height / 2 - f.getHeight() / 2;
+		f.setLocation(x, y);
+		return f;
+	}
+}
