e26b0e2a96c5059c55be0539d62ceedd3081e4de
jeremiah <jeremiah@linux-pc>
2018-10-03 01:51:52 -0400

    Added centerFrame method

--- src/net/net16/jeremiahlowe/shared/SwingUtility.java
+++ src/net/net16/jeremiahlowe/shared/SwingUtility.java
@@ -13,4 +13,13 @@ public class SwingUtility {
 		f.setLocation(x, y);
 		return f;
 	}
+	public static final Frame centerFrame(Frame f) {
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = ge.getDefaultScreenDevice();
+		Rectangle screen = gd.getDefaultConfiguration().getBounds();
+		int x = screen.x + screen.width / 2 - f.getWidth() / 2;
+		int y = screen.y + screen.height / 2 - f.getHeight() / 2;
+		f.setLocation(x, y);
+		return f;
+	}
 }
