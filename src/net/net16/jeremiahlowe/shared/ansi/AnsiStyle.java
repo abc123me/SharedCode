4174aaaa7f1aecbd6cbaea30b3e26dbe87baedd1
Jeremiah Lowe <abc123me@github.com>
2019-05-24 14:11:46 -0400

    Updated ANSI

--- src/net/net16/jeremiahlowe/shared/ansi/AnsiStyle.java
+++ src/net/net16/jeremiahlowe/shared/ansi/AnsiStyle.java
@@ -1,20 +1,91 @@
 package net.net16.jeremiahlowe.shared.ansi;
 
-@Deprecated
-public enum AnsiStyle{
-	Reset(0), Bright(1), Dark(2), Italic(3), Underlined(4), Strikethrough(9);
-	private int id;
-	private AnsiStyle(int id){this.id = id;}
-	public int getAnsi(){return id;}
-	public static AnsiStyle fromID(int id) {
-		switch(id){
-			case 0: return Reset;
-			case 1: return Bright;
-			case 2: return Dark;
-			case 3: return Italic;
-			case 4: return Underlined;
-			case 9: return Strikethrough;
-			default: return null;
+public class AnsiStyle{
+	public static final String ANSI_ESCAPE = "\u001B[";
+	
+	public static final int FORMAT_ITALIC      = 0x01;
+	public static final int FORMAT_UNDERLINE   = 0x02;
+	public static final int FORMAT_BRIGHTEN    = 0x04;
+	public static final int FORMAT_STRIKE      = 0x08;
+	public static final int FORMAT_CONCEAL     = 0x10;
+	public static final int FORMAT_DARKEN      = 0x20;
+	public static final int FORMAT_SWAP_COLORS = 0x40;
+	
+	public static final int ANSI_RESET         = 0;
+	public static final int ANSI_BRIGHTEN      = 1;
+	public static final int ANSI_DARKEN        = 2;
+	public static final int ANSI_ITALIC        = 3;
+	public static final int ANSI_UNDERLINE     = 4;
+	public static final int ANSI_SWAP          = 7;
+	public static final int ANSI_CONCEAL       = 8;
+	public static final int ANSI_STRIKE        = 9;
+	public static final int ANSI_N_BRIGHTEN    = 21;
+	public static final int ANSI_N_DEFAULT     = 22;
+	public static final int ANSI_N_ITALIC      = 23;
+	public static final int ANSI_N_UNDERLINE   = 24;
+	public static final int ANSI_N_SWAP        = 27;
+	public static final int ANSI_N_CONCEAL     = 28;
+	public static final int ANSI_N_STRIKE      = 29;
+	
+	private static final int N_FORMAT_ITALIC      = ~FORMAT_ITALIC;
+	private static final int N_FORMAT_UNDERLINE   = ~FORMAT_UNDERLINE;
+	private static final int N_FORMAT_BRIGHTEN    = ~FORMAT_BRIGHTEN;
+	private static final int N_FORMAT_DEFAULT     = ~(FORMAT_BRIGHTEN | FORMAT_DARKEN);
+	private static final int N_FORMAT_STRIKE      = ~FORMAT_STRIKE;
+	private static final int N_FORMAT_CONCEAL     = ~FORMAT_CONCEAL;
+	private static final int N_FORMAT_DARKEN      = ~FORMAT_DARKEN;
+	private static final int N_FORMAT_SWAP_COLORS = ~FORMAT_SWAP_COLORS;
+	
+	private int formattingMask;
+	
+	public AnsiStyle() { this(0); }
+	public AnsiStyle(int val) { formattingMask = val; }
+	public AnsiStyle(AnsiStyle val) { this(val.formattingMask); }
+	
+	public void interpretAnsiCode(int code) {
+		switch(code) {
+			//Normal flags
+			case ANSI_RESET:       formattingMask = 0; break;
+			case ANSI_BRIGHTEN:    formattingMask |= FORMAT_BRIGHTEN; break;
+			case ANSI_DARKEN:      formattingMask |= FORMAT_DARKEN; break;
+			case ANSI_ITALIC:      formattingMask |= FORMAT_ITALIC; break;
+			case ANSI_UNDERLINE:   formattingMask |= FORMAT_UNDERLINE; break;
+			case ANSI_SWAP:        formattingMask |= FORMAT_SWAP_COLORS; break;
+			case ANSI_CONCEAL:     formattingMask |= FORMAT_CONCEAL; break;
+			case ANSI_STRIKE:      formattingMask |= FORMAT_STRIKE; break;
+			// Disable flags
+			case ANSI_N_BRIGHTEN:  formattingMask &= N_FORMAT_BRIGHTEN; break;
+			case ANSI_N_DEFAULT:   formattingMask &= N_FORMAT_DEFAULT; break;
+			case ANSI_N_ITALIC:    formattingMask &= N_FORMAT_ITALIC; break;
+			case ANSI_N_UNDERLINE: formattingMask &= N_FORMAT_UNDERLINE; break;
+			case ANSI_N_SWAP:      formattingMask &= N_FORMAT_SWAP_COLORS; break;
+			case ANSI_N_CONCEAL:   formattingMask &= N_FORMAT_CONCEAL; break;
+			case ANSI_N_STRIKE:    formattingMask &= N_FORMAT_STRIKE; break;
+			default: break;
 		}
+	}
+	
+	public void reset() { formattingMask = 0; }
+	
+	public boolean italic() {    return (formattingMask & FORMAT_ITALIC)      != 0; }
+	public boolean underline() { return (formattingMask & FORMAT_UNDERLINE)   != 0; }
+	public boolean brighten() {  return (formattingMask & FORMAT_BRIGHTEN)    != 0; }
+	public boolean darken() {    return (formattingMask & FORMAT_DARKEN)      != 0; }
+	public boolean conceal() {   return (formattingMask & FORMAT_CONCEAL)     != 0; }
+	public boolean strike() {    return (formattingMask & FORMAT_STRIKE)      != 0; }
+	public boolean swap() {      return (formattingMask & FORMAT_SWAP_COLORS) != 0; }
+	
+	
+	@Override public String toString() {
+		return toString(true);
+	}
+	public String toString(boolean reset) {
+		String out = ANSI_ESCAPE + (reset ? "0;" : "");
+		if(italic()) out += "3;";
+		if(brighten()) out += "1;";
+		if(darken()) out += "2;";
+		if(underline()) out += "4;";
+		if(strike()) out += "7;";
+		return out + "m";
 	}
 }
--- src/net/net16/jeremiahlowe/shared/ansi/AnsiTextPane.java
+++ src/net/net16/jeremiahlowe/shared/ansi/AnsiTextPane.java
@@ -5,23 +5,40 @@ import javax.swing.text.*;
 import java.awt.*;
 
 public class AnsiTextPane extends JTextPane{
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		JFrame f = new JFrame();
-		f.setBounds(800, 100, 500, 300);
+		f.setBounds(0, 0, 1920, 1080);
 		JScrollPane sp = new JScrollPane();
 		AnsiTextPane pane = new AnsiTextPane();
 		f.setContentPane(sp);
-		for(int j = 0; j < 10; j++)
-			for(int i = 0; i < 8; i++) 
-				pane.append(ANSI_ESCAPE + "[" + j + ";3" + i + "m[" + i + ", " + j + "]: Hello world!" + ANSI_CLEAR + "\n");
-		pane.append(ANSI_ESCAPE + "[38;2;125;125;125mTEST GREY");
+		//for(int j = 0; j < 10; j++)
+		//	for(int i = 0; i < 8; i++) 
+		//		pane.append(ANSI_ESCAPE + "[" + j + ";3" + i + "m[" + i + ", " + j + "]: Hello world!" + ANSI_CLEAR + "\n");
 		sp.setViewportView(pane);
+		pane.setAutoscrolls(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
-	}
+		int xi = pane.getFontMetrics(pane.getFont()).stringWidth(" ");
+		int yi = pane.getFontMetrics(pane.getFont()).getHeight();
+		float w = pane.getWidth() - 2 * xi, h = pane.getHeight() - 2 * yi;
+		float asp = w / h; asp /= 2;
+		float a = 3;
+		pane.resetAttributes();
+		for(int y = 0; y < h; y += yi) {
+			for(int x = 0; x < w; x += xi) {
+				int r = 255 / 2 + ((int)Math.round(255 * Math.sqrt(Math.cos((y / h) * 2 * a * asp * Math.PI)))) / 2;
+				int g = (int)Math.round(255 * Math.sqrt(Math.sin(((x * y) / (w * h)) * 2 * a * asp * Math.PI)));
+				int b = 255 / 2 + ((int)Math.round(255 * Math.sqrt(Math.cos((x / w) * 2 * a * Math.PI)))) / 2;
+				String astr = "[48;2;" + r + ';' + g + ';' + b + "m ";
+				//System.out.println(astr);
+				pane.append(ANSI_ESCAPE + astr); 
+			}
+			pane.append("\n");
+		}
+	}*/
 	
-	public static final char ANSI_ESCAPE = 0x1B;
-	public static final String ANSI_CLEAR = ANSI_ESCAPE + "[0m";
+	public static final String ANSI_ESCAPE = "\u001B[";
+	public static final String ANSI_CLEAR = ANSI_ESCAPE + "0m";
 	
 	private static final int DEFAULT_FOREGROUND = 0;
 	private static final int DEFAULT_BACKGROUND = 7;
@@ -53,6 +70,7 @@ public class AnsiTextPane extends JTextPane{
 	private int currentForeground;
 	private int currentBackground;
 	private int formattingMask;
+	private boolean autoscrolls = false;
 	
 	public AnsiTextPane(){
 		super();
@@ -77,27 +95,34 @@ public class AnsiTextPane extends JTextPane{
 		formattingMask = 0;
 	}
 	public void setGlobalBackground(Color c) { 
-		setGlobalColor(DEFAULT_BACKGROUND, c); 
+		setGlobalColor(DEFAULT_BACKGROUND, c, true); 
 		super.setBackground(c);
 		repaint(); validate();
 	}
 	public void setGlobalForeground(Color c) { 
-		setGlobalColor(DEFAULT_FOREGROUND, c); 
+		setGlobalColor(DEFAULT_FOREGROUND, c, true); 
 		super.setForeground(c);
 		repaint(); validate();
 	}
+	@Override public void setAutoscrolls(boolean s) { autoscrolls = s; }
+	@Override public boolean getAutoscrolls() { return autoscrolls; }
 	public Color getDarkBackground() { return darkColorMap[DEFAULT_BACKGROUND]; }
 	public Color getDarkForeground() { return darkColorMap[DEFAULT_FOREGROUND]; }
 	public Color getBrightBackground() { return brightColorMap[DEFAULT_BACKGROUND]; }
 	public Color getBrightForeground() { return brightColorMap[DEFAULT_FOREGROUND]; }
 	public Color getCurrentBackground() { return getCurrentColorMap()[DEFAULT_BACKGROUND]; }
 	public Color getCurrentForeground() { return getCurrentColorMap()[DEFAULT_FOREGROUND]; }
-	public void setGlobalColor(int c, Color to) {
-		if(c < 0 || c >= defaultColorMap.length)
-			throw new RuntimeException("Color at position " + c + " does not exist");
-		defaultColorMap[c] = to;
-		brightColorMap[c] = to.brighter();
-		darkColorMap[c] = to.darker();
+	public void setGlobalColor(int pos, Color color, boolean adjustBrightness) {
+		if(pos < 0 || pos >= defaultColorMap.length)
+			throw new RuntimeException("Color at position " + pos + " does not exist");
+		defaultColorMap[pos] = color;
+		if(adjustBrightness) {
+			brightColorMap[pos] = color.brighter();
+			darkColorMap[pos] = color.darker();
+		} else {
+			brightColorMap[pos] = color;
+			darkColorMap[pos] = color;
+		}
 	}
 	
 	public void append(String s){
@@ -126,6 +151,16 @@ public class AnsiTextPane extends JTextPane{
 		atb(text);
 		text = "";
 	}
+	@Override public void setText(String txt) {
+		clear();
+		append(txt); //No way to "set" the text all at once since the style must be dynamically changed based on the text
+	}
+	public void clear() {
+		super.setText("");
+	}
+	public void setRawText(String txt) {
+		super.setText(txt);
+	}
 	
 	private Color[] getCurrentColorMap() {
 		if((formattingMask & FORMAT_DARKEN) != 0) return darkColorMap;
@@ -147,33 +182,26 @@ public class AnsiTextPane extends JTextPane{
 		int len = getDocument().getLength(); 
 		try {getDocument().insertString(len, s, aset);} 
 		catch (BadLocationException e) {e.printStackTrace();}
+		if(autoscrolls) setCaretPosition(getDocument().getLength());
 	}
 	private void handleSpecial(int i) {
 		float caretSpeed = -1;
 		switch(i) {
 			//Normal flags
-			case 1: formattingMask |= FORMAT_BRIGHTEN; break;
-			case 2: formattingMask |= FORMAT_DARKEN; break;
-			case 3: formattingMask |= FORMAT_ITALIC; break;
-			case 4: formattingMask |= FORMAT_UNDERLINE; break;
 			case 5: caretSpeed = slowCaretRate; break;
 			case 6: caretSpeed = fastCaretRate; break;
-			case 7: formattingMask |= FORMAT_SWAP_COLORS; break;
-			case 8: formattingMask |= FORMAT_CONCEAL; break;
-			case 9: formattingMask |= FORMAT_STRIKE; break;
 			// Disable flags
-			case 21: formattingMask &= ~FORMAT_BRIGHTEN; break;
-			case 22: formattingMask &= ~(FORMAT_DARKEN | FORMAT_BRIGHTEN); break;
-			case 23: formattingMask &= ~FORMAT_ITALIC; break;
-			case 24: formattingMask &= ~FORMAT_UNDERLINE; break;
 			case 25: caretSpeed = 0; break;
-			case 28: formattingMask &= ~FORMAT_CONCEAL; break;
-			case 29: formattingMask &= ~FORMAT_STRIKE; break;
-			case 27: formattingMask &= ~FORMAT_SWAP_COLORS; break;
 			default: break;
 		}
-		if(caretSpeed >= 0 && getCaret() != null)
-			getCaret().setBlinkRate(Math.round(caretSpeed));
+		Caret c = getCaret();
+		if(caretSpeed >= 0 && c != null) {
+			if(caretSpeed > 0) {
+				c.setVisible(true);
+				c.setBlinkRate(Math.round(caretSpeed));
+			}
+			else if(caretSpeed == 0) c.setVisible(false);
+		}
 	}
 	private int customColor(String[] parts, int index, boolean foreground) {
 		if(index + 1 >= parts.length) return 0;
@@ -205,7 +233,18 @@ public class AnsiTextPane extends JTextPane{
 			else if(i == 39) currentForeground = DEFAULT_FOREGROUND;
 			else if(i == 49) currentBackground = DEFAULT_BACKGROUND;
 			else if(i == 38) j += customColor(parts, j, true); 
-			else if(i == 48) j += customColor(parts, j, true); 
+			else if(i == 48) j += customColor(parts, j, false); 
 		}
+	}
+
+	public void appendStyled(String s, int... styles) {
+		String prefix = "";
+		if(styles.length > 0){
+			prefix = "\u001B[";
+			for(int style : styles)
+				prefix += style + ";";
+			prefix = prefix.substring(0, prefix.length() - 1) + "m";
+		}
+		append("\u001B[0m" + prefix + s + "\u001B[0m");
 	}	
 }
