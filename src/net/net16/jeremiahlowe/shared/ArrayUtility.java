abaf840bda5f2c77da49e25d0824978ee41d6909
Jeremiah Lowe <abc123me@github.com>
2020-01-27 15:57:10 -0500

    Utility fuctions

--- src/net/net16/jeremiahlowe/shared/ArrayUtility.java
+++ src/net/net16/jeremiahlowe/shared/ArrayUtility.java
@@ -1,5 +1,7 @@
 package net.net16.jeremiahlowe.shared;
 
+import java.io.PrintStream;
+
 public class ArrayUtility {
 	@SafeVarargs
 	public static <T> boolean anyOf(T any, T... of){
@@ -7,5 +9,16 @@ public class ArrayUtility {
 			if(any == t) return true;
 		}
 		return false;
+	}
+
+	public static <T extends Object> void printArray(T[] r) {
+		printArray(System.out, r, ", ", System.lineSeparator());
+	}
+	public static <T extends Object> void printArray(PrintStream to, T[] r, String sep, String end) {
+		String out = "";
+		for(T p : r)
+			out += "\"" + p.toString() + "\"" + sep;
+		out = out.substring(0, out.length() - sep.length()) + end;
+		to.print(out);
 	}
 }
