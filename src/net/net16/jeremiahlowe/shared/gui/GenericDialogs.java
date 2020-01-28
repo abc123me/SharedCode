01cc2ab62a5e1e88a6d0e9c1ae87d70c9c4845ef
Jeremiah Lowe <abc123me@github.com>
2020-01-27 22:04:08 -0500

    Added showTimeDialog

--- src/net/net16/jeremiahlowe/shared/gui/GenericDialogs.java
+++ src/net/net16/jeremiahlowe/shared/gui/GenericDialogs.java
@@ -1,5 +1,6 @@
 package net.net16.jeremiahlowe.shared.gui;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.InetAddress;
@@ -11,24 +12,27 @@ import net.net16.jeremiahlowe.shared.SwingUtility;
 
 public class GenericDialogs {
 	public static void main(String[] args) {
-		InetAddress ip = showHostnameDialog();
+		/*InetAddress ip = showHostnameDialog();
 		if(ip == null) System.out.println("No IP!");
-		else System.out.println(ip);
+		else System.out.println(ip);*/
+		long l = showDelayDialogMS(10, 100);
+		System.out.println(l);
 	}
 	
-	public static InetAddress showHostnameDialog() {
-		JFrame jf = new JFrame("Please enter a hostname/IP!");
-		jf.setLayout(new BoxLayout(jf.getContentPane(), BoxLayout.Y_AXIS));
+	private static JFrame initJF(String t, int w, int h) {
+		JFrame jf = new JFrame(t);
 		jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		jf.setSize(500, 200);
-
-		
+		jf.setSize(w, h);
+		SwingUtility.centerJFrame(jf);
+		return jf;
+	}
+	public static InetAddress showHostnameDialog() {
+		JFrame jf = initJF("Please enter a hostname/IP!", 300, 200);
+		jf.setLayout(new BoxLayout(jf.getContentPane(), BoxLayout.Y_AXIS));
 		JLabel lblFormat = new JLabel("Hostname/IP address:");
 		jf.add(lblFormat);
-		
 		JTextField field = new JTextField("localhost");
 		jf.add(field);
-		
 		JButtonTracked btnCancel = new JButtonTracked("Cancel");
 		JButtonTracked btnSubmit = new JButtonTracked("OK");
 		Box controlBx = Box.createHorizontalBox();
@@ -37,8 +41,6 @@ public class GenericDialogs {
 		jf.add(controlBx);
 		
 		jf.setVisible(true);
-		SwingUtility.centerJFrame(jf);
-		
 		InetAddress addr = null;
 		try {
 			while(addr == null) {
@@ -51,6 +53,62 @@ public class GenericDialogs {
 		} catch (InterruptedException e) {}
 		jf.dispose();
 		return addr;
+	}
+	public static Long showDelayDialogMS(long min, long max) { return showTimeDialog(min, max, "delay"); }
+	public static Long showTimeDialog(long min, long max, String verb) {
+		JFrame jf = initJF("Please choose a " + verb + "!", 300, 200);
+		jf.setLayout(new BorderLayout());
+		
+		JPanel selBox = new JPanel();
+		selBox.setLayout(new GridLayout(2, 2));
+		JComboBox<ETimeUnit> unitSpinner = new JComboBox<ETimeUnit>(ETimeUnit.values());
+		JSpinner amountSpinner = new JSpinner(new SpinnerNumberModel(0, Integer.MIN_VALUE, Integer.MAX_VALUE, 10));
+		selBox.add(new JLabel("Amount")); selBox.add(amountSpinner);
+		selBox.add(new JLabel("Unit"));   selBox.add(unitSpinner);
+		jf.add(selBox, BorderLayout.CENTER);
+		
+		JButtonTracked btnCancel = new JButtonTracked("Cancel");
+		JButtonTracked btnSubmit = new JButtonTracked("OK");
+		Box controlBx = Box.createHorizontalBox();
+		controlBx.add(btnSubmit);
+		controlBx.add(btnCancel);
+		jf.add(controlBx, BorderLayout.SOUTH);
+		
+		jf.setVisible(true);
+		Long out = null;
+		try {
+			while(out == null && jf.isDisplayable()) {
+				if(btnSubmit.wasPressed()) {
+					Integer spr = (Integer) amountSpinner.getValue();
+					if(spr == null)	continue;
+					long val = spr.longValue();
+					String err = null;
+					ETimeUnit unit = (ETimeUnit) unitSpinner.getSelectedItem();
+					val *= unit.milliseconds;
+					if(val < min || val > max) {
+						err = "Value is ";
+						long lim = min;
+						if(val < min) err += "less then minimum of ";
+						else { err = "over the maximum of "; lim = max; }
+						err += String.format("%f %s", lim / (float) unit.milliseconds, unit.toString().toLowerCase());
+					}
+					if(err == null) out = Long.valueOf(val);
+					else JOptionPane.showMessageDialog(jf, err);
+		
+				} else if(btnCancel.wasPressed()) break;
+				Thread.sleep(50);
+			}
+		} catch (InterruptedException e) {}
+		jf.dispose();
+		return out;
+	}
+}
+enum ETimeUnit{
+	Milliseconds(1), Seconds(1000), Minutes(60000), Hours(3600000), Days(86400000);
+	
+	public final long milliseconds;
+	private ETimeUnit(long ms) {
+		milliseconds = ms;
 	}
 }
 class JButtonTracked extends JButton implements ActionListener {
